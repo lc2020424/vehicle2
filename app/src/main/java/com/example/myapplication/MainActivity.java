90a90f2a7de93521c020264a86b7b25ab045e713
lc2020424 <lwh6408@163.com>
2020-06-29 14:03:44 +0800

    获取下拉框中车辆ID  问题描述字符串 解决办法字符串

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,33 +12,35 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView view ;
+    private TextView view;
     private Spinner spinner;
     private ArrayAdapter<String> adapter;
 
-    private EditText editText1 =null;
-    private EditText editText2 =null;
+    private EditText editText1 = null;
+    private EditText editText2 = null;
     private Button btn1 = null;
-    String str1="";
+    String strq = "";
+    String stra = "";
+    String strid = "536";
 
-    private static final String[] m={"536","537","538","539","540","541","542","543","544","545",
-            "546","547","548","549","550","551","552","553","554","555","556","557","558","559","560","561","562","563","564","565"};
+    private static final String[] m = {"536", "537", "538", "539", "540", "541", "542", "543", "544", "545",
+            "546", "547", "548", "549", "550", "551", "552", "553", "554", "555", "556", "557", "558", "559", "560", "561", "562", "563", "564", "565"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = (Button)findViewById(R.id.button1);
+        btn1 = (Button) findViewById(R.id.button1);
         //用EditText前，请先:import android.widget.EditText;
         //获取文本框1的文本
-        editText1 =(EditText)findViewById(R.id.q1);
-        editText2 =(EditText)findViewById(R.id.a1);
+        editText1 = (EditText) findViewById(R.id.q1);
+        editText2 = (EditText) findViewById(R.id.a1);
 
         view = (TextView) findViewById(R.id.spinnerText);
         spinner = (Spinner) findViewById(R.id.Spinner01);
         //将可选内容与ArrayAdapter连接起来
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,m);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, m);
 
         //设置下拉列表的风格
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -55,21 +57,25 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                str1=editText1.getText().toString();
-                editText2.setText(str1.toCharArray(), 0, str1.length());
+                strq = editText1.getText().toString();
+                stra = editText2.getText().toString();
+
+                 editText2.setText(strid.toCharArray(), 0, strid.length());
             }
         });
     }
-        //使用数组形式操作
-        class SpinnerSelectedListener implements AdapterView.OnItemSelectedListener {
 
-            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
-                                       long arg3) {
-                view.setText("当前车辆是："+m[arg2]);
-            }
+    //使用数组形式操作
+    class SpinnerSelectedListener implements AdapterView.OnItemSelectedListener {
 
-            public void onNothingSelected(AdapterView<?> arg0) {
-            }
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+                                   long arg3) {
+            strid =  m[arg2];
+            view.setText("当前车辆是：" + m[arg2]);
         }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+        }
+    }
 
 }
