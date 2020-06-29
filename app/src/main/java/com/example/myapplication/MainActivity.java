a9970fd22dc3b0e7334cad661603373cd7596495
lc2020424 <lwh6408@163.com>
2020-06-29 13:06:47 +0800

    初次提交

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //用EditText前，请先:import android.widget.EditText;
+        //获取文本框1的文本
+        String str1="";
+        EditText editText1 =(EditText)findViewById(R.id.q1);
+        str1=editText1.getText().toString();
+
+        String str2="";
+        EditText editText2 =(EditText)findViewById(R.id.a1);
+        str2=editText1.getText().toString();
+
+    }
+}
